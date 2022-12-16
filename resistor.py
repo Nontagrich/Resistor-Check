87587f33fcc49680fab2ddca5ddea3faecc6ed23
Nontagrich <113359860+Nontagrich@users.noreply.github.com>
2022-12-16 19:47:19 +0700

    Update resistor.py

--- resistor.py
+++ resistor.py
@@ -5,4 +5,10 @@ bar3 = input("Enter Bar3: ")
 bar4 = input("Enter Bar4: ")
 
 #Resistor tier
-tier = [('black',)]
+tier = [
+    {'black':0,'brown':1,'red':2,'orange':3,'yellow':4,'green':5,'blue':6,'purple':7},
+    {'black':1,'brown':10,'red':10**2,'orange':10**3,'yellow':10**4,'green':10**5,'blue':10**6,'gold':10**-1,'silver':10**}
+    ]
+
+#tier[0][bar1]
+print(tier[1]['gold'])
