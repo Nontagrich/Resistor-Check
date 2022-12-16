2505a2c2f766337172f39ef1674cdcb0bcf30a93
Nontagrich <113359860+Nontagrich@users.noreply.github.com>
2022-12-16 20:18:20 +0700

    Update resistor.py

--- resistor.py
+++ resistor.py
@@ -6,8 +6,8 @@ bar4 = input("Enter Bar4: ")
 
 #tier
 tierI = [
-    {'black':0,'brown':1,'red':2,'orange':3,'yellow':4,'green':5,'blue':6,'purple':7},
-    {'black':1,'brown':10,'red':10**2,'orange':10**3,'yellow':10**4,'green':10**5,'blue':10**6,'gold':10**-1,'silver':10**-2}
+    {'black':0,'brown':1,'red':2,'orange':3,'yellow':4,'green':5,'blue':6,'purple':7,'gray':8,'white':9},
+    {'black':1,'brown':10,'red':10**2,'orange':10**3,'yellow':10**4,'green':10**5,'blue':10**6,'purple':10**7,'gray':10**8,'white':10**9,'gold':10**-1,'silver':10**-2}
     ]
 
 
@@ -16,10 +16,12 @@ resis = ( (tierI[0][bar1]*10) + tierI[0][bar2] ) * tierI[1][bar3]
 
 #discrepancy
 tierII = [
-    {'brown':(0.01)*resis,'red':(0.02)*resis,'gold':(0.05)*resis,'silver':(0.1)*resis,'non':(0.2)*resis}
+    {'brown':(0.01)*resis,'red':(0.02)*resis,'yellow':(0.05)*resis,'green':(0.005)*resis,'blue':(0.0025),'gold':(0.05)*resis,'silver':(0.1)*resis,'non':(0.2)*resis},
+    {'brown':'1%','red':'2%','gold':(0.05)*resis,'silver':(0.1)*resis,'non':(0.2)*resis}
 ]
 
 result_1 = resis - tierII[0][bar4]
 result_2 = resis + tierII[0][bar4]
 
-print()
+print("Resistance value of the Resistor: ")
+print(str(resis) + ' + ' + str(int(tierII[1][bar4])) + '%')
