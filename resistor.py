c1265e2d897e93079776dbeba20b613277df79f5
Nontagrich <113359860+Nontagrich@users.noreply.github.com>
2022-12-16 20:32:31 +0700

    Update resistor.py

--- resistor.py
+++ resistor.py
@@ -14,14 +14,17 @@ tierI = [
 #resistor
 resis = ( (tierI[0][bar1]*10) + tierI[0][bar2] ) * tierI[1][bar3]
 
+
 #discrepancy
 tierII = [
     {'brown':(0.01)*resis,'red':(0.02)*resis,'yellow':(0.05)*resis,'green':(0.005)*resis,'blue':(0.0025)*resis,'purple':(0.001)*resis,'gray':(0.0005)*resis,'gold':(0.05)*resis,'silver':(0.1)*resis,'non':(0.2)*resis},
-    {'brown':'1%','red':'2%','gold':(0.05)*resis,'silver':(0.1)*resis,'non':(0.2)*resis}
+    {'brown':'1%','red':'2%','yellow':'5%','green':'0.5%','blue':'0.25%','purple':'0.1%','gray':'0.05%','gold':'5%','silver':'10%','non':'20%'}
 ]
 
+
+#result
 result_1 = resis - tierII[0][bar4]
 result_2 = resis + tierII[0][bar4]
 
 print("Resistance value of the Resistor: ")
-print(str(resis) + ' + ' + str(int(tierII[1][bar4])) + '%')
+print('= ' + str(resis) + ' + ' + str(tierII[1][bar4]+' Ω'))
